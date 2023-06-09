063f20374f44a4abe84a9e2759c350464459a78c
DPrudnikov <dima1911@mail.ru>
2023-06-09 22:02:01 +0300

    changed Ex43

--- Ex43/Program.cs
+++ Ex43/Program.cs
@@ -13,7 +13,14 @@ double k1 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите число k2 ");
 double k2 = Convert.ToDouble(Console.ReadLine());
 
+if (k1!=k2)
+{
 double x = -(b1-b2)/(k1-k2);
 double y = k1 * x + b1;
 
 Console.WriteLine($"Точка пересечения двух прямых: -> ({x};{y})");
+}
+else if (k1==k2)
+{
+    Console.WriteLine("Прямые параллельны ");
+}
